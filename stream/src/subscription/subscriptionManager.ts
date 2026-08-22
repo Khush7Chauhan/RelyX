8a36c328ef4b90da5856de3dc9cb4e47a1957efd
khush chauhan <khushchauhan2006@gmail.com>
2026-08-22 23:34:59 +0530

    updated

---
+++ stream/src/subscription/subscriptionManager.ts
@@ -0,0 +1,26 @@
+export class SubscriptionManager {
+  private topicSubscriptions = new Map<string, Set<string>>();
+
+  public subscribe(clientId: string, topics: string[]) {
+    for (const topic of topics) {
+      if (!this.topicSubscriptions.has(topic)) {
+        this.topicSubscriptions.set(topic, new Set());
+      }
+      this.topicSubscriptions.get(topic)!.add(clientId);
+    }
+  }
+
+  public unsubscribeAll(clientId: string) {
+    for (const [topic, clients] of this.topicSubscriptions.entries()) {
+      clients.delete(clientId);
+      if (clients.size === 0) {
+        this.topicSubscriptions.delete(topic);
+      }
+    }
+  }
+
+  public getSubscribers(topic: string): string[] {
+    const clients = this.topicSubscriptions.get(topic);
+    return clients ? Array.from(clients) : [];
+  }
+}
